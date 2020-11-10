654a299239453ad3b175559faa02c61ed17ab7b1
comet1112 <sunearth.moon@outlook.com>
2020-11-10 22:31:05 +0700

    change host

--- config/database.php
+++ config/database.php
@@ -46,7 +46,7 @@ return [
         'mysql' => [
             'driver' => 'mysql',
             'url' => env('DATABASE_URL'),
-            'host' => env('DB_HOST', 'https://www.db4free.net'),
+            'host' => env('DB_HOST', 'db4free.net'),
             'port' => env('DB_PORT', '3306'),
             'database' => env('DB_DATABASE', 'photoquotes'),
             'username' => env('DB_USERNAME', 'photoquotes'),
