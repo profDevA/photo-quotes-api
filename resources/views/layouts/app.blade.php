f7dda30dd79d40babec77b12461c06da40336dc3
comet1112 <sunearth.moon@outlook.com>
2020-11-14 00:20:52 +0700

    update

--- resources/views/layouts/app.blade.php
+++ resources/views/layouts/app.blade.php
@@ -139,15 +139,6 @@
                         </li>
                         @endif
 
-                        <a class="dropdown-item" href="{{ route('logout') }}" onclick="event.preventDefault();
-                                                     document.getElementById('logout-form').submit();">
-                            {{ __('Logout') }}
-                        </a>
-
-                        <form id="logout-form" action="{{ route('logout') }}" method="POST" class="d-none">
-                            @csrf
-                        </form>
-
                     </ul>
                 </nav>
                 <!-- /.sidebar-menu -->
